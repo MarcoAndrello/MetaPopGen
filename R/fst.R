c578ee0c52f2a4668edb45a222fb2e090a6f461e
Marco Andrello <marco.andrello@gmail.com>
2020-07-15 14:56:24 +0200

    Fst, function and manual

--- R/fst.R
+++ R/fst.R
@@ -1,4 +1,5 @@
-# Generic FST function
+# Generic FST function for single locus and multi-locus simulations
+
 fst <- function(N) {
   
   # Define basic variables
@@ -33,3 +34,48 @@ fst <- function(N) {
   return(fst)
   
 }
+
+fst_multilocus <- function(N, init.par) {
+  if(length(dim(N))!=2) stop("N must have two dimensions: genotype and group")
+  nLoc <- length(init.par$allele_vec) # Number of loci
+  num_groups <- dim(N)[2]             # Number of groups
+  locus_names <- paste0("Locus",LETTERS[1:nLoc])
+  
+  # Number of individuals for each group
+  n_group <- colSums(N) # number of individuals for each group
+  
+  # Genotype arrays for the total population
+  N_T <- rowSums(N)
+  
+  # H_S per locus per group
+  H_S_group <- array(NA,c(nLoc,num_groups))
+  dimnames(H_S_group) <- list(Locus = locus_names,
+                              Deme = colnames(N))
+  for(i.group in 1 : num_groups) {
+    H_S_group[,i.group] <- het_exp(N[,i.group], init.par)
+  }
+  
+  # H_S per locus as weighted means of the H_S per group
+  H_S <- rep(0,nLoc)
+  for(i.locus in 1 : nLoc) {
+    H_S[i.locus] <- weighted.mean(H_S_group[i.locus,],n_group)
+  }
+  
+  # H_T per locus
+  H_T <- het_exp(N_T, init.par)
+  
+  # Fst per locus
+  fst <- rep(NA,nLoc)
+  for(i.locus in 1 : nLoc) {
+    fst[i.locus] <- (H_T[i.locus] - H_S[i.locus]) / H_T[i.locus]
+  }
+  
+  # Mean over loci
+  fst_mean <- (mean(H_T) - mean(H_S)) / mean(H_T)
+  
+  # Out
+  out <- c(fst,fst_mean)
+  names(out) <- c(locus_names,"Mean")
+  out
+}
+
