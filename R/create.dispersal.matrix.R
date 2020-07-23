ee3aeb3ab394348b0515c9ec75e881cda249e46c
Marco Andrello <marco.andrello@gmail.com>
2020-07-23 10:41:26 +0200

    Added create.dispersal matrix for island models

--- R/create.dispersal.matrix.R
+++ R/create.dispersal.matrix.R
@@ -1,7 +1,12 @@
 # Create dispersal matrix from deme coordinates
-create.dispersal.matrix = function(x,a,longlat = FALSE) {
+create.dispersal.coord = function(x,a,longlat = FALSE) {
   require(sp)
   dist.mat <- spDists(x,longlat=longlat)
   y   <- 0.5*a*exp(-a*abs(dist.mat))
+  y
+}
+create.dispersal.IM = function(n,m) {
+  y <- matrix(m/(n-1),nrow=n,ncol=n)
+  diag(y) <- (1-m)
   y
 }
